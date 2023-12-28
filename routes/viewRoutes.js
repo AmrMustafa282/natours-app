3fcaa84a3190b03f96eb45689b85d3a65003009a
AmrMustafa <amrdarwish115@gmail.com>
2023-12-28 05:07:57 +0200

    fix / route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -12,10 +12,10 @@ const {
   getMyTours,
 } = require('./../controllers/viewController');
 
-// const {createBookingCheckout} = require('./../controllers/bookingController')
+const {createBookingCheckout} = require('./../controllers/bookingController')
 // router.use(isLoggedIn);
 
-// router.get('/',createBookingCheckout, isLoggedIn,getOverview);
+router.get('/',createBookingCheckout, isLoggedIn,getOverview);
 router.get('/tour/:slug', isLoggedIn,getTour);
 router.get('/auth/login',isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
